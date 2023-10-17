92f1f86b6d71521463ce0e5df9590922db43ca44
maryameb <personalmaryamebrahimi@gmail.com>
2023-10-17 16:40:46 +0330

    use env-variables

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-console.log("connecting to port 5000");
+require("dotenv").config();
+console.log(process.env.PORT);
+console.log(process.env.BASE_URL);
 
-axios.get("https://api.koo.ir");
+// axios.get("https://api.koo.ir");
